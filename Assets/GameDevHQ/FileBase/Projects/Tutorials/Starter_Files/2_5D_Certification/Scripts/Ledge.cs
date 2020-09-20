7541c056b262e4611ab7effdf0c7127b0f3471a6
Samir Georgy <57589276+samirgeorgy@users.noreply.github.com>
2020-09-20 21:01:37 +0200

    Video Steps Commit

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification/Scripts/Ledge.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ledge : MonoBehaviour
+{
+    #region Private Variables
+
+    [SerializeField] private Vector3 _ledgePosition, _standPosition;
+
+    #endregion
+
+    #region public properties
+
+    /// <summary>
+    /// Gets the ledge hand position
+    /// </summary>
+    public Vector3 LedgePosition
+    {
+        get { return _ledgePosition; }
+    }
+
+    /// <summary>
+    /// Gets the ledge stand position
+    /// </summary>
+    public Vector3 StandPosition
+    {
+        get { return _standPosition; }
+    }
+
+    #endregion
+
+    #region Unity Functions
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ledge_Grab_Checker"))
+        {
+            var player = other.transform.parent.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.GrabLedge(this);
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification/Scripts/Player.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Private Variables
+
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _gravity = 1.0f;
+
+    private Vector3 _direction;
+    private CharacterController _controller;
+    private Animator _anim;
+    private bool _jumping = false;
+    private bool _onLedge = false;
+
+    private Ledge _activeLedge;
+
+    #endregion
+
+    #region Unity Functions
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+
+        if (_onLedge)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _anim.SetTrigger("ClimbUp");
+            }
+        }
+    }
+
+    #endregion
+
+    #region Supporting Functions
+
+    private void CalculateMovement()
+    {
+        if (_controller.isGrounded)
+        {
+            if (_jumping == true)
+            {
+                _jumping = false;
+                _anim.SetBool("Jumping", _jumping);
+            }
+
+            float horizontalMovement = Input.GetAxisRaw("Horizontal");
+            _direction = new Vector3(0, 0, horizontalMovement) * _speed;
+            _anim.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+
+            if (horizontalMovement > 0)
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            else if (horizontalMovement < 0)
+                transform.eulerAngles = new Vector3(0, 180, 0);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _direction.y += _jumpHeight;
+                _jumping = true;
+                _anim.SetBool("Jumping", _jumping);
+            }
+        }
+
+        _direction.y -= _gravity * Time.deltaTime;
+        _controller.Move(_direction * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Grab a ledge
+    /// </summary>
+    public void GrabLedge(Ledge currentLedge)
+    {
+        _controller.enabled = false;
+        _anim.SetBool("GrabLedge", true);
+        _anim.SetBool("Jumping", false);
+        _anim.SetFloat("Speed", 0.0f);
+        transform.position = currentLedge.LedgePosition;
+        _onLedge = true;
+        _activeLedge = currentLedge;
+    }
+
+    /// <summary>
+    /// Climb up
+    /// </summary>
+    public void ClimbUpComplete()
+    {
+        transform.position = _activeLedge.StandPosition;
+        _anim.SetBool("GrabLedge", false);
+        _controller.enabled = true;
+    }
+
+    #endregion
+}
